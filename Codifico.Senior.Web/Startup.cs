0630400ea4b99ba274175253844345c4498367ff
Juan Carlos <jcnustes@gmail.com>
2017-12-10 19:44:07 -0500

    First Commit

---
+++ Codifico.Senior.Web/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Owin;
+using Microsoft.Owin;
+[assembly: OwinStartup(typeof(Codifico.Senior.Web.Startup))]
+
+namespace Codifico.Senior.Web
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Any connection or hub wire up and configuration should go here
+            app.MapSignalR();
+        }
+    }
+}
